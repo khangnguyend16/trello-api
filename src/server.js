e395c1ec2c1f899370fbcc922b0a404a71f42eee
Khang <khangnguyend16@gmail.com>
2025-05-07 00:23:38 +0700

    1st commit

---
+++ src/server.js
@@ -0,0 +1,14 @@
+import express from 'express'
+
+const app = express()
+
+const hostname = 'localhost'
+const port = 8017
+
+app.get('/', function (req, res) {
+    res.send('<h1>Hello World Nodejs</h1>')
+})
+
+app.listen(port, hostname, () => {
+    console.log(`Hello World, server running at http://${hostname}:${port}/`);
+})
