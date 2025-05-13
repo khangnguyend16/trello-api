364bbb1a368f6f3ad2919ddc48daa6a28160ad3d
Khang <khangnguyend16@gmail.com>
2025-05-13 22:45:16 +0700

    config cors

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,7 @@
 // Những domain được phép truy cập tới tài nguyên của Server
 export const WHITELIST_DOMAINS = [
-  "http://localhost:5173",
+  // Không cần localhost nữa vì ở file cors đã luôn luôn cho phép môi trường dev
+  // "http://localhost:5173",
   // ... (domain chính thức sau deploy)
 ];
 
